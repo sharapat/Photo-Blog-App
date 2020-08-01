ad0f984f88b2395c3c2b33d80fbb6ad766d65903
Sharapat Kalabaev <kalabaevshj@gmail.com>
2020-08-01 14:04:06 +0500

    abs

--- app/src/main/java/com/example/socialnetwork/ui/sign/LoginActivity.kt
+++ app/src/main/java/com/example/socialnetwork/ui/sign/LoginActivity.kt
@@ -53,6 +53,7 @@ class LoginActivity : AppCompatActivity() {
         if (user != null) {
             val intent = Intent(this, MainActivity::class.java)
             startActivity(intent)
+            finish()
         }
     }
 }
